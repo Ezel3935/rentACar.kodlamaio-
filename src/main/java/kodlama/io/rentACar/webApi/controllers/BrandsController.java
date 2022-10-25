937fb26f67e32bcbe9a916fea4c90c716b3a40c0
ezel inan <ezel.acer88@gmail.com>
2022-10-26 00:17:34 +0300

    Initial commit

---
+++ src/main/java/kodlama/io/rentACar/webApi/controllers/BrandsController.java
@@ -0,0 +1,27 @@
+package kodlama.io.rentACar.webApi.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlama.io.rentACar.business.abstracts.BrandService;
+import kodlama.io.rentACar.entities.concretes.Brand;
+
+@RestController//annotation
+@RequestMapping("/api/brands")//ulaşım için kullanılır
+public class BrandsController {
+	private BrandService brandservice;
+
+	@Autowired
+	public BrandsController(BrandService brandservice) {
+		this.brandservice = brandservice;
+	}
+	
+	@GetMapping("/getAll")
+	public List<Brand> getAll(){
+		return brandservice.getAll();
+	}
+}
